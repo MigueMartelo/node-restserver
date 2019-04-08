9355aa7637454aa8970a40fe2dc7e9efc70dded8
MigueMartelo <miguemartelo1031@gmail.com>
2019-04-08 17:51:12 -0500

    Updates

--- server/server.js
+++ server/server.js
@@ -20,7 +20,9 @@ app.use(express.static(path.resolve(__dirname, '../public')));
 // Configuración global de rutas
 app.use(require('./routes/index'));
 
-mongoose.connect(process.env.URLDB, (err, resp) => {
+mongoose.connect(process.env.URLDB,
+    { useNewUrlParser: true, useCreateIndex: true },
+    (err, resp) => {
     if (err) throw err;
 
     console.log('Base de datos ONLINE');
